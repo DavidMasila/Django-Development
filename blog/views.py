1eb069830aae7fe7c4ae0ab85fdf3e358b21d540
David Masila <davidmasila97@gmail.com>
2023-05-05 08:09:53 +0300

    Django Development

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
-
+@login_required
 def home(request):
     context = {
         'posts': Post.objects.all()
